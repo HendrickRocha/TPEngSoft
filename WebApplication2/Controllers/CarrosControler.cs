d1b0223335c21bbba125635db48806d4ca74817b
unisanta <unisanta@M216-13>
2023-10-27 21:36:44 -0300

    Add project files.

---
+++ WebApplication2/Controllers/CarrosControler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CarrosController : Controller
+    {
+        private readonly List<Carros> carros = new List<Carros>();
+
+        public CarrosController()
+        {
+            carros.Add(new Carros()
+            {
+                Nome = "Fit",
+                Placa = "ABC1234",
+                Marca = "Honda",
+                Cambio = "Automático"
+            });
+            carros.Add(new Carros()
+            {
+                Nome = "Corsa",
+                Placa = "DFG1234",
+                Marca = "Chevrolet",
+                Cambio = "Manual"
+            });
+        }
+
+        public IActionResult Index()
+        {
+            return View(carros);
+        }
+        public IActionResult Details(string id)
+        {
+            var carro = carros.Find(c => c.Placa == id);
+            if (carro == null) return NotFound();
+
+            return View(carro);
+        }
+    }
+}
---
+++ WebApplication2/Models/Carros.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication2.Models
+{
+    public class Carros
+    {
+        public string Nome { get; set; } = string.Empty;
+
+        public string Placa { get; set; } = string.Empty;
+
+        public string Marca { get; set; } = string.Empty;
+
+        public string Cambio { get; set; } = string.Empty;
+
+
+    }
+}
